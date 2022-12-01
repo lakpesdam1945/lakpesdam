5d66be9448176e02a19733b8a6841a7683f32e2f
lakpesdam1945 <lakpesdam.rck@gmail.com>
2022-12-02 01:08:11 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <h1>Edi Koswara</h1>
+      <div className="container mx-auto p-4 flex items-center ">
+        <img
+          src="https://images.unsplash.com/photo-1594409855476-29909f35c73c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFkaWVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt="Lakpesdam Rancaekek"
+          className="object-cover bg-cover w-full min-h-screen rounded-lg"
+        />
+      </div>
     </>
   );
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      body: ["Poppins", '"Open Sans"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
